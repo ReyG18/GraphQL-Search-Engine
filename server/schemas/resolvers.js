6d336027b7f5232f39713470e03e2db9c148ce1a
Rey G. <reyanna.garibay@gmail.com>
2024-07-31 03:38:19 -0500

    fixed AuthenticationError bug

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Book } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-errors');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
